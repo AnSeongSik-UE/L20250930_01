fe65cb742725fb25fd29edc684d898987a9dd3c7
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-30 11:46:08 +0900

    비트연산자 활용 Q01 풀이
    비트연산자 활용 Q01 풀이

--- main.cpp
+++ main.cpp
@@ -4,24 +4,33 @@ using namespace std;
 
 int main()
 {
-	unsigned char Number = 0b00000001;
-	
-	//Number = Number << 8; //0b0000 0000 -> 0
+	int N = 0;
+	unsigned long long X = 0;
+	unsigned long long Result = 0;
 
-	Number = Number << 1; //0b0000 0010 -> 2
-	Number = Number << 1; //0b0000 0100 -> 4
-	Number = Number << 1; //0b0000 1000 -> 8
-	Number = Number << 1; //0b0001 0000 -> 16
-
-	Number = Number << 1; //0b0100 0000 -> 64
-	Number = Number << 1; //0b1000 0000 -> 128
-	Number = Number << 1; //0b0000 0000 -> 0
-
-	Number = Number >> 1; //0b0000 1000 -> 8
-	Number = Number >> 1; //0b0000 0100 -> 4
-	Number = Number >> 1; //0b0000 0010 -> 2
-	Number = Number >> 1; //0b0000 0001 -> 1
-	Number = Number >> 1; //0b0000 0000 -> 0
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> X;
+		unsigned long long NPOT = 2;
+		for (int j = 1; j < 64; ++j)
+		{
+			if (NPOT >= X)
+			{
+				if (i == 0)
+				{
+					Result = NPOT;
+				}
+				else
+				{
+					Result = Result ^ NPOT;
+				}
+				break;
+			}
+			NPOT = NPOT << 1;
+		}
+	}
+	cout << Result << endl;
 
 	return 0;
 }
