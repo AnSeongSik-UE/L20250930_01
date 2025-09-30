a38e494fe8f2880cd5096fa71dba057e82081765
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-30 14:11:59 +0900

    Image 클래스화
    Image 클래스화

--- main.cpp
+++ main.cpp
@@ -2,35 +2,17 @@
 
 using namespace std;
 
+class Image
+{
+	int X = 0;
+	int Y = 0;
+	int R = 0;
+	int G = 0;
+	int B = 0;
+};
+
 int main()
 {
-	int N = 0;
-	unsigned long long X = 0;
-	unsigned long long Result = 0;
-
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-	{
-		cin >> X;
-		unsigned long long NPOT = 2;
-		for (int j = 1; j < 64; ++j)
-		{
-			if (NPOT >= X)
-			{
-				if (i == 0)
-				{
-					Result = NPOT;
-				}
-				else
-				{
-					Result = Result ^ NPOT;
-				}
-				break;
-			}
-			NPOT = NPOT << 1;
-		}
-	}
-	cout << Result << endl;
 
 	return 0;
 }
