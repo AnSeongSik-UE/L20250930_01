a5cbe14dc9105fcd06a6d86eccdc85cf762b4060
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-30 15:05:25 +0900

    테이블 클래스화01
    테이블 클래스화01

--- main.cpp
+++ main.cpp
@@ -2,17 +2,46 @@
 
 using namespace std;
 
-class Image
+class State
 {
-	int X = 0;
-	int Y = 0;
-	int R = 0;
-	int G = 0;
-	int B = 0;
+public:
+	int ID;
+	string StateName;
+};
+
+class Transition
+{
+public:
+	int CurrentState;
+	string Condition;
+	int NextState;
+};
+
+class FSM
+{
+public:
+	FSM()
+	{
+		States[0].ID = 1;
+		States[0].StateName = "배회";
+		States[1].ID = 2;
+		States[1].StateName = "추격";
+		States[2].ID = 3;
+		States[2].StateName = "공격";
+		States[3].ID = 4;
+		States[3].StateName = "죽음";
+	}
+	State States[4];
+	Transition Transitions[5];
+
+	State Transit(State InState, string Condition);
 };
 
 int main()
 {
+	FSM MyFSM;
+	MyFSM.Transit();
+
 
 	return 0;
 }
