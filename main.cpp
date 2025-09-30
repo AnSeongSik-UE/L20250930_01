621541feeb59f29d9663894a1f89245f4b041c20
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-30 11:33:26 +0900

    비트시프트
    비트시프트

--- main.cpp
+++ main.cpp
@@ -2,31 +2,26 @@
 
 using namespace std;
 
-#define WorldStatic		0b00000001
-#define WorldDynamic	0b00000010
-#define Pawn			0b00000100
-#define PhysicsBody		0b00001000
-
 int main()
 {
-	unsigned char MyActorType = Pawn | PhysicsBody;
+	unsigned char Number = 0b00000001;
 	
-	// ´À¸²
-	//string MyActorTypes = "Pawn, Physics";
-	//if (MyActorTypes.find("Pawn"))
-	//{
-	//	'P' == 'P' && 'a' == 'a';
-	//}
+	//Number = Number << 8; //0b0000 0000 -> 0
 
-	if (MyActorType & Pawn)
-	{
-		cout << "Pawn" << endl;
-	}
-	else
-	{
-		cout << "Not Pawn" << endl;
-	}
+	Number = Number << 1; //0b0000 0010 -> 2
+	Number = Number << 1; //0b0000 0100 -> 4
+	Number = Number << 1; //0b0000 1000 -> 8
+	Number = Number << 1; //0b0001 0000 -> 16
 
+	Number = Number << 1; //0b0100 0000 -> 64
+	Number = Number << 1; //0b1000 0000 -> 128
+	Number = Number << 1; //0b0000 0000 -> 0
+
+	Number = Number >> 1; //0b0000 1000 -> 8
+	Number = Number >> 1; //0b0000 0100 -> 4
+	Number = Number >> 1; //0b0000 0010 -> 2
+	Number = Number >> 1; //0b0000 0001 -> 1
+	Number = Number >> 1; //0b0000 0000 -> 0
 
 	return 0;
 }
